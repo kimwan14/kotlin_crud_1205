9d9529986187bf4eb308c86b857c58398661f1a4
Wandroid <eung990@naver.com>
2023-12-14 22:27:49 +0900

    5

--- app/src/main/java/com/example/kotiln_1205_crud/MainActivity.kt
+++ app/src/main/java/com/example/kotiln_1205_crud/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.kotiln_1205_crud.databinding.ActivityMainBinding
 
@@ -15,7 +16,12 @@ class  MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        setupActionBarWithNavController(findNavController(R.id.fragment))
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            setupActionBarWithNavController(findNavController(R.id.fragment))
+        } else {
+            // ActionBar가 null일 경우 대처 방법을 여기에 추가
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
