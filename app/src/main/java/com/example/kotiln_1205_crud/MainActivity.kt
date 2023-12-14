4f33f1a9213a5ddf5ece9015f579281e2fdaa9b3
Wandroid <eung990@naver.com>
2023-12-14 22:56:48 +0900

    5

--- app/src/main/java/com/example/kotiln_1205_crud/MainActivity.kt
+++ app/src/main/java/com/example/kotiln_1205_crud/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotiln_1205_crud
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -16,11 +17,15 @@ class  MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val TAG:String = "Log"
         val actionBar = supportActionBar
         if (actionBar != null) {
             setupActionBarWithNavController(findNavController(R.id.fragment))
+            Log.d(TAG,"실행중")
         } else {
             // ActionBar가 null일 경우 대처 방법을 여기에 추가
+            Log.d(TAG,"오류")
+
         }
     }
 
